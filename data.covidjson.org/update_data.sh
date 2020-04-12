5d0d5f662922db73ff9674b98beb53a5292cf461
Ilkka Rinne <ilkka.rinne@spatineo.com>
2020-04-12 21:33:31 +0300

    Added content-type object headers for the data files

--- data.covidjson.org/update_data.sh
+++ data.covidjson.org/update_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 java -jar ./converter/target/covidjson-converter-1.0-SNAPSHOT.jar
-aws s3 cp ./out/fin_newInfectionsTimeseriesByHealthCareDistrict.geojson s3://data.covidjson.org/
-aws s3 cp ./out/fin_totalInfectionsByHealthCareDistrict.geojson s3://data.covidjson.org/
+aws s3 cp ./out/fin_newInfectionsTimeseriesByHealthCareDistrict.geojson s3://data.covidjson.org/ --content-type application/geo+json
+aws s3 cp ./out/fin_totalInfectionsByHealthCareDistrict.geojson s3://data.covidjson.org/ --content-type application/geo+json
 aws s3 cp index.html s3://data.covidjson.org/
 aws cloudfront create-invalidation --distribution-id E2OHUOG1J75B5R --paths '/*'
