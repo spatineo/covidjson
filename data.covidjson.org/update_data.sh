3651b22c295f40e8c4b78825f0a3d6e95c85d175
Ilkka Rinne <ilkka.rinne@spatineo.com>
2020-04-13 13:11:03 +0300

    Improved update script

--- data.covidjson.org/update_data.sh
+++ data.covidjson.org/update_data.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-java -jar ./converter/target/covidjson-converter-1.0-SNAPSHOT.jar
-aws s3 cp ./out/fin_newInfectionsTimeseriesByHealthCareDistrict.geojson s3://data.covidjson.org/ --content-type application/geo+json
-aws s3 cp ./out/fin_totalInfectionsByHealthCareDistrict.geojson s3://data.covidjson.org/ --content-type application/geo+json
-aws s3 cp index.html s3://data.covidjson.org/
-aws cloudfront create-invalidation --distribution-id E2OHUOG1J75B5R --paths '/*'
+JAVA=/usr/bin/java
+AWS=$HOME/.local/bin/aws
+AWS_PROFILE=covidjson
+$JAVA -jar ./converter/target/covidjson-converter-1.0-SNAPSHOT.jar
+$AWS s3 cp ./out/fin_newInfectionsTimeseriesByHealthCareDistrict.geojson s3://data.covidjson.org/ --profile $AWS_PROFILE --content-type application/geo+json
+$AWS s3 cp ./out/fin_totalInfectionsByHealthCareDistrict.geojson s3://data.covidjson.org/ --profile $AWS_PROFILE --content-type application/geo+json
+$AWS s3 cp index.html s3://data.covidjson.org/
+$AWS cloudfront create-invalidation --distribution-id E2OHUOG1J75B5R --paths '/*'
