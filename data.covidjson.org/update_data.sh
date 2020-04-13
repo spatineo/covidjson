b80a3dd554b00bc3b40760d68f1f66b233dc8342
Ilkka Rinne <ilkka.rinne@spatineo.com>
2020-04-13 13:22:01 +0300

    Improved update script

--- data.covidjson.org/update_data.sh
+++ data.covidjson.org/update_data.sh
@@ -2,8 +2,10 @@
 JAVA=/usr/bin/java
 AWS=$HOME/.local/bin/aws
 AWS_PROFILE=covidjson
+cd $HOME/git/covidjson/data.covidjson.org
 $JAVA -jar ./converter/target/covidjson-converter-1.0-SNAPSHOT.jar
 $AWS s3 cp ./out/fin_newInfectionsTimeseriesByHealthCareDistrict.geojson s3://data.covidjson.org/ --profile $AWS_PROFILE --content-type application/geo+json
 $AWS s3 cp ./out/fin_totalInfectionsByHealthCareDistrict.geojson s3://data.covidjson.org/ --profile $AWS_PROFILE --content-type application/geo+json
 $AWS s3 cp index.html s3://data.covidjson.org/
 $AWS cloudfront create-invalidation --distribution-id E2OHUOG1J75B5R --paths '/*'
+cd -
